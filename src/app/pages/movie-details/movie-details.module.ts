4b46a871465586c76e7fca80269120e85341178a
plovecky <p_lovecky@utb.cz>
2022-12-21 12:00:24 +0100

    nahrani projektu

--- src/app/pages/movie-details/movie-details.module.ts
+++ src/app/pages/movie-details/movie-details.module.ts
@@ -4,17 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { FolderPageRoutingModule } from './folder-routing.module';
+import { MovieDetailsPageRoutingModule } from './movie-details-routing.module';
 
-import { FolderPage } from './folder.page';
+import { MovieDetailsPage } from './movie-details.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    FolderPageRoutingModule
+    MovieDetailsPageRoutingModule
   ],
-  declarations: [FolderPage]
+  declarations: [MovieDetailsPage]
 })
-export class FolderPageModule {}
+export class MovieDetailsPageModule {}
