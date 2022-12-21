7201530a23b60c876993ac486ecab595ea3e29fc
plovecky <p_lovecky@utb.cz>
2022-12-21 12:13:44 +0100

    vymazani api klice

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 export const environment = {
   production: true,
-  apiKey: '8b2cad9825184c2a0a1591df16c67cf0',
+  apiKey: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
   baseUrl: 'https://api.themoviedb.org/3',
   images: 'https://image.tmdb.org/t/p'
 };
